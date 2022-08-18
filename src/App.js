b5f0df17ea0555115d8da0e016075a94e3713e3b
yakubalkis <yakupalkiis@gmail.com>
2022-08-18 18:26:02 +0300

    Route-path and Header added to App.js.

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Header from "./components/Header"
+import Cart from "./pages/Cart"
+import Photos from "./pages/Photos"
+import {Routes, Route} from 'react-router-dom'
+function App() {    
+    return (
+        <div>
+            <Header />
+            <h1>Home Page</h1>
+           
+
+            <Routes>
+                <Route path="/" element={<Photos/>} />
+                <Route path="/cart" element={<Cart />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default App
