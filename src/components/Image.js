716e79842616b2017d7b97d94aa59ace67bd7d78
yakubalkis <yakupalkiis@gmail.com>
2022-08-19 00:14:44 +0300

    Added state for favorite and cart icons on photos

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,20 @@
-import React from "react"
+import React, {useState} from "react"
 
 function Image({className, img}) {
+
+    const [hovered, setHovered] = useState(false)
+   
+    const heartIcon = <i onMouseEnter={() => {setHovered(true)}} className="ri-heart-line favorite"></i>
+    const cartIcon =  <i onMouseEnter={() => {setHovered(true)}} className="ri-add-circle-line cart"></i>
+    
     return (
         <div className={`${className} image-container`}>
-            <img src={img.url} className="image-grid" />
+            <img onMouseEnter={()  => {setHovered(true)}} 
+                 onMouseLeave={() => {setHovered(false)}}
+                 src={img.url} 
+                 className="image-grid" />
+                {hovered && heartIcon}
+                {hovered && cartIcon}
         </div>
     )
 }
