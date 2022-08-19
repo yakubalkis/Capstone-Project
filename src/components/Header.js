14fe30f8054148d7081bf77760ddda97ae82ea14
yakubalkis <yakupalkiis@gmail.com>
2022-08-19 14:33:35 +0300

    Functionality added to cart logo in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
-import React from "react"
+import React, { useContext } from "react"
 import {Link} from 'react-router-dom'
+import {AppContext} from '../context/Context'
+
 function Header() {
+    const {cartItems} = useContext(AppContext)
+
+    function cartLogo(){
+        if(cartItems.length>0){
+            return (<i className="ri-shopping-cart-fill ri-fw ri-2x"></i>)
+        }
+        else return (<i className="ri-shopping-cart-line ri-fw ri-2x"></i>)
+    }
+
     return (
         <header>
             <Link to='/'> <h2>Pic Some</h2> </Link>
-            <Link to='/cart' > <i className="ri-shopping-cart-line ri-fw ri-2x"></i> </Link>
+            <Link to='/cart' > {cartLogo()} </Link>
             
         </header>
     )
