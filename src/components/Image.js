fb3f216fea29d282210be8ff817087b4a0412ee6
yakubalkis <yakupalkiis@gmail.com>
2022-08-19 13:15:41 +0300

    Funtionalities added to favorite and card icons

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,21 +1,71 @@
-import React, {useState} from "react"
+import React, {useContext, useEffect, useState} from "react"
+import{ PropTypes} from 'prop-types'
+import {AppContext} from '../context/Context'
 
 function Image({className, img}) {
 
+
+    const {addToCart, toggleFavorite, cartItems,removeFromCart} = useContext(AppContext)
     const [hovered, setHovered] = useState(false)
-   
-    const heartIcon = <i onMouseEnter={() => {setHovered(true)}} className="ri-heart-line favorite"></i>
-    const cartIcon =  <i onMouseEnter={() => {setHovered(true)}} className="ri-add-circle-line cart"></i>
+    
+    function heartIcon(){
+        if(img.isFavorite){
+            return (
+            <i 
+            className="ri-heart-fill favorite" 
+            onClick={() => {toggleFavorite(img.id)}}> 
+            </i>)
+        } else if(hovered){
+            return   (
+            <i 
+            onMouseEnter={() => {setHovered(true)}} 
+            className="ri-heart-line favorite" 
+            onClick={() => {toggleFavorite(img.id)}}> 
+            </i>)
+        }
+    }
+    
+    function cartIcon(){
+        const isInCart = cartItems.some(item => item.id === img.id)
+        if(isInCart){
+            return (
+            <i 
+                className="ri-shopping-cart-fill cart"
+                onClick={() => {removeFromCart(img.id)}}
+                >
+                </i>)
+        }
+        else if(hovered){
+            return (
+            <i
+                onMouseEnter={() => {setHovered(true)}} 
+                className="ri-add-circle-line cart"
+                onClick={() => {addToCart(img)}}> 
+            </i>)
+        }
+    }
+
     
     return (
         <div className={`${className} image-container`}>
-            <img onMouseEnter={()  => {setHovered(true)}} 
+            <img onMouseEnter={() => {setHovered(true)}} 
                  onMouseLeave={() => {setHovered(false)}}
                  src={img.url} 
-                 className="image-grid" />
-                {hovered && heartIcon}
-                {hovered && cartIcon}
+                 className="image-grid" 
+                 alt=""
+            />
+
+                {heartIcon()}
+                {cartIcon()}
         </div>
     )
+}
+Image.propTypes= {
+    img:PropTypes.shape({
+        id:PropTypes.string.isRequired,
+        url:PropTypes.string.isRequired,
+        isFavorite:PropTypes.bool
+    }),
+    className:PropTypes.string
 }
 export default Image
