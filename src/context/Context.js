d869ec1fb9d68ee3a370ae99c624a520a1824dfa
yakubalkis <yakupalkiis@gmail.com>
2022-08-19 13:16:20 +0300

    Created functions for card and favorite icons

--- src/context/Context.js
+++ src/context/Context.js
@@ -5,17 +5,37 @@ const AppContext = createContext()
 function AppContextProvider({children}){
 
     const [allPhotos, setAllPhotos] = useState([])
+    const [cartItems, setCartItems] = useState([])
+    
 
     useEffect(() => { 
         fetch('https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json')
         .then(res => res.json())
         .then(data => setAllPhotos(data))},[] )
-    
+        
+        function addToCart(img){
+            setCartItems(prevState =>  [...prevState,img ])
+        }
 
-        console.log(allPhotos)
+        function removeFromCart(id){
+            setCartItems(prevItems => prevItems.filter(item => item.id !== id))
+        }
+       
+     
+        function toggleFavorite(id){
+            const newState = allPhotos.map(obj =>  {
+                if(obj.id === id){
+                    return {...obj, isFavorite: !obj.isFavorite}
+                }
+                return obj
+            })
+            setAllPhotos(newState)
+        }
+        
+        
 
     return(
-        <AppContext.Provider value={{allPhotos}}>  {/*allPhotos: allPhotos */}
+        <AppContext.Provider value={{allPhotos,  toggleFavorite, addToCart, cartItems, removeFromCart}}>  {/*allPhotos: allPhotos */}
             {children}
         </AppContext.Provider>
     )
