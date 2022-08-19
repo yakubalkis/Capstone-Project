41be54c0af357aea40809df419fb1a059836b2a4
yakubalkis <yakupalkiis@gmail.com>
2022-08-19 21:35:57 +0300

    Added mouse events to trash icon

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,17 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import PropTypes from 'prop-types'
 import {AppContext} from '../context/Context'
 
 
 function CartItem({item}) {
 
+
     const {removeFromCart} = useContext(AppContext)
+    const [className,setClassName]= useState("ri-delete-bin-line")
     return (
         <div>
-            <i className="ri-delete-bin-line" onClick={() => removeFromCart(item.id)} ></i>
-            <img alt="" src={item.url} width='130px'  />
+            <i className={className} 
+               onClick={() => removeFromCart(item.id)}
+               onMouseEnter={() => setClassName("ri-delete-bin-fill")}
+               onMouseLeave={() => setClassName("ri-delete-bin-line")}
+            ></i>
+            <img alt="" src={item.url} width='130px' height='130px' />
             <p>$5.99</p>
         </div>
     )
 }
-
+CartItem.propTypes = {
+    item:PropTypes.shape({
+        url:PropTypes.string.isRequired
+    })
+}
 export default CartItem
